1d054e441bae02e6265ae31e9e70663eb11332b5
Felipe Cerda <felipecerdabarra@gmail.com>
2020-05-12 02:04:39 +0200

    feat: Updated layout of the app

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -10,6 +10,48 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+/*
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the View that shows the Songs category
+        TextView songs = (TextView) findViewById(R.id.songs);
+
+        // Set a click listener on that View
+        songs.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the Songs category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the Songs Activity
+                Intent songsIntent = new Intent(MainActivity.this, SongsActivity.class);
+
+                // Start the new activity
+                startActivity(songsIntent);
+            }
+        });
+
+        // Find the View that shows the Podcasts category
+        TextView podcasts = (TextView) findViewById(R.id.podcasts);
+
+        // Set a click listener on that View
+        podcasts.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the Podcasts category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the Podcasts Activity
+                Intent podcastsIntent = new Intent(MainActivity.this, PodcastsActivity.class);
+
+                // Start the new activity
+                startActivity(podcastsIntent);
+            }
+        });
+    }
+}
+*/
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,36 +60,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Creates the ArrayList to store Songs
+        ArrayList<Song> songs = new ArrayList<Song>();
+
+        //Add songs
+        songs.add(new Song("Family", "The Interrupters"));
+        songs.add(new Song("Roots Radical", "Rancid"));
+        songs.add(new Song("Roots Radical", "Rancid"));
+        songs.add(new Song("Roots Radical", "Rancid"));
+        songs.add(new Song("Roots Radical", "Rancid"));
+        songs.add(new Song("Roots Radical", "Rancid"));
+        songs.add(new Song("Roots Radical", "Rancid"));
+        songs.add(new Song("Roots Radical", "Rancid"));
+        songs.add(new Song("Roots Radical", "Rancid"));
+        songs.add(new Song("Roots Radical", "Rancid"));
+        songs.add(new Song("Roots Radical", "Rancid"));
+        songs.add(new Song("Roots Radical", "Rancid"));
+
+        SongAdapter adapter = new SongAdapter(this, songs);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
         // Find the View that shows the Podcasts category
         TextView podcasts = (TextView) findViewById(R.id.podcasts);
 
         // Set a click listener on that View
-        podcasts.setOnClickListener(new View.OnClickListener() {
+        podcasts.setOnClickListener(new OnClickListener() {
             // The code in this method will be executed when the Podcasts category is clicked on.
             @Override
             public void onClick(View view) {
-                // Create a new intent to open the PodcastsActivity
+                // Create a new intent to open the Podcasts Activity
                 Intent podcastsIntent = new Intent(MainActivity.this, PodcastsActivity.class);
 
                 // Start the new activity
                 startActivity(podcastsIntent);
             }
         });
-
-        // Find the View that shows the Songs category
-        TextView songs = (TextView) findViewById(R.id.songs);
-
-        // Set a click listener on that View
-        songs.setOnClickListener(new View.OnClickListener() {
-            // The code in this method will be executed when the Songs category is clicked on.
-            @Override
-            public void onClick(View view) {
-                // Create a new intent to open the SongsActivity
-                Intent songsIntent = new Intent(MainActivity.this, SongsActivity.class);
-
-                // Start the new activity
-                startActivity(songsIntent);
-            }
-        });
     }
 }
+
--- app/src/main/java/com/example/musicplayer/PodcastsActivity.java
+++ app/src/main/java/com/example/musicplayer/PodcastsActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
-
+import android.view.View.OnClickListener;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
-
+/*
 public class PodcastsActivity extends AppCompatActivity {
 
     @Override
@@ -17,7 +17,7 @@ public class PodcastsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.song_list);
 
-        //Creates the ArrayList to store Songs
+        //Creates the ArrayList to store podcast titles
         ArrayList<Song> podcasts = new ArrayList<Song>();
 
         //Add songs
@@ -29,6 +29,47 @@ public class PodcastsActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list);
 
         listView.setAdapter(adapter);
+
+    }
+
+}
+*/
+
+public class PodcastsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Creates the ArrayList to store podcast titles
+        ArrayList<Song> podcasts = new ArrayList<Song>();
+
+        //Add songs
+        podcasts.add(new Song("APG 423 - Down the Hatch", "Airline Pilot Guy"));
+        podcasts.add(new Song("Episode 84: Completing the trifecta", "AvTalk - Aviation Podcast"));
+
+        SongAdapter adapter = new SongAdapter(this, podcasts);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+        // Find the View that shows the Songs category
+        TextView songs = (TextView) findViewById(R.id.songs);
+
+        // Set a click listener on that View
+        songs.setOnClickListener(new OnClickListener() {
+            // The code in this method will be executed when the Songs category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Create a new intent to open the Songs Activity
+                Intent songsIntent = new Intent(PodcastsActivity.this, MainActivity.class);
+
+                // Start the new activity
+                startActivity(songsIntent);
+            }
+        });
 
     }
 
